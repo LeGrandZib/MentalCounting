23a3130ae1361c138c9419faae1dfa5fbc66acb0
ELOY <theo.eloy@etud.u-picardie.fr>
2022-02-02 13:14:40 +0100

    Suite Interface

---
+++ app/src/main/java/com/example/mentalcounting/Score_Activity.java
@@ -0,0 +1,13 @@
+package com.example.mentalcounting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Score_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
