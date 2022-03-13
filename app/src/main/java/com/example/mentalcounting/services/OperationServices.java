7089bf7e66ba8f8a96f99afc36de0a5e66764c1b
ELOY <theo.eloy@etud.u-picardie.fr>
2022-03-13 22:39:34 +0100

    Création modèle contenant l'opération

---
+++ app/src/main/java/com/example/mentalcounting/services/OperationServices.java
@@ -0,0 +1,9 @@
+package com.example.mentalcounting.services;
+
+import com.example.mentalcounting.models.operationModel;
+
+public class OperationServices {
+    public operationModel genrateOperation(){
+
+    }
+}
